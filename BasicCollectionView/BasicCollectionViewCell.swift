d19519eaf730f86f561e423d623dc2863618f5ea
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-05-14 01:01:30 -0300

    Initial Commit

---
+++ BasicCollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by Christian Manzaraz on 14/05/2023.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var label: UILabel!
+    
+}
